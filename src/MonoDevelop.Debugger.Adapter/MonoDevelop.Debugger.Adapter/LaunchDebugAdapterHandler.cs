effe738eef6316e47fd5c0af941a32440b1f0e07
Matt Ward <ward.matt@gmail.com>
2018-03-31 15:48:07 +0100

    Add Launch Debug Adapter menu
    The menu does not do much. It allows a launch.json file to be
    selected. The file selected is read and converted into a class.
    The launch.json file format supported is not the same as the one
    that VS Code uses. The format currently only allows one launch
    configuration to be specified:
    
    {
      "$adapter": "bin/Debug/SampleDebugAdapter.exe",
    
      "type": "sample",
      "request": "launch",
      "program": "src/sample/SampleDebugAdapter/SampleScripts/TestScript.txt"
    }

---
+++ src/MonoDevelop.Debugger.Adapter/MonoDevelop.Debugger.Adapter/LaunchDebugAdapterHandler.cs
@@ -0,0 +1,74 @@
+﻿//
+// LaunchDebugAdapterHandler.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2018 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using MonoDevelop.Components;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui.Dialogs;
+
+namespace MonoDevelop.Debugger.Adapter
+{
+	class LaunchDebugAdapterHandler : CommandHandler
+	{
+		protected override void Run ()
+		{
+			try {
+				OnLaunchDebugAdapter ();
+			} catch (Exception ex) {
+				LoggingService.LogError ("Failed to launch debug adapter", ex);
+				MessageService.ShowError (ex.Message);
+			}
+		}
+
+		void OnLaunchDebugAdapter ()
+		{
+			FilePath launchJsonFile = SelectLaunchJsonFile ();
+			if (launchJsonFile.IsNotNull) {
+				DebugAdapterService.LaunchAdapter (launchJsonFile);
+			}
+		}
+
+		FilePath SelectLaunchJsonFile ()
+		{
+			var dialog = new OpenFileDialog ();
+			dialog.Action = FileChooserAction.Open;
+			dialog.Title = GettextCatalog.GetString ("Launch Debug Adapter");
+
+			// 'launch.json' as a file filter does not work so use a wildcard version.
+			dialog.AddFilter (GettextCatalog.GetString ("Launch Files (launch.json)"), "*launch*.json");
+			dialog.AddFilter (GettextCatalog.GetString ("Json Files (*.json)"), "*.json");
+			dialog.AddAllFilesFilter ();
+
+			if (dialog.Run ()) {
+				return dialog.SelectedFile;
+			}
+
+			return null;
+		}
+	}
+}
---
+++ src/MonoDevelop.Debugger.Adapter/MonoDevelop.Debugger.Adapter/MinimalLaunchJson.cs
@@ -0,0 +1,80 @@
+﻿//
+// MinimalLaunchJson.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2018 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using Newtonsoft.Json;
+using MonoDevelop.Core;
+using System.IO;
+using System;
+using System.Diagnostics;
+
+namespace MonoDevelop.Debugger.Adapter
+{
+	/// <summary>
+	/// { 
+	///     "type": "mock",
+	///     "request": "launch",
+	///     "program": "c:\\path\\to\\your\\folder\\MockDebug.txt"
+	/// }
+	/// </summary>
+	class MinimalLaunchJson
+	{
+		[JsonProperty ("type")]
+		public string Type { get; set; }
+
+		[JsonProperty ("request")]
+		public string Request { get; set; }
+
+		[JsonProperty ("program")]
+		public string Program { get; set; }
+
+		[JsonProperty ("$adapter")]
+		public string Adapter { get; set; }
+
+		public static MinimalLaunchJson Read (FilePath fileName)
+		{
+			string json = File.ReadAllText (fileName);
+			var launchJson = JsonConvert.DeserializeObject<MinimalLaunchJson> (json);
+
+			launchJson.Program = FixRelativePath (fileName.ParentDirectory, launchJson.Program);
+			launchJson.Adapter = FixRelativePath (fileName.ParentDirectory, launchJson.Adapter);
+
+			return launchJson;
+		}
+
+		static string FixRelativePath (FilePath baseDirectory, string fileName)
+		{
+			if (string.IsNullOrEmpty (fileName)) {
+				return fileName;
+			}
+
+			if (Path.IsPathRooted (fileName)) {
+				return fileName;
+			}
+
+			return Path.Combine (baseDirectory, fileName);
+		}
+	}
+}
